252830fbbcffc8b20f8bf7d6dee13efe41eb2742
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-06 21:54:58 -0600

    working, not pretty

--- src/components/Towers.js
+++ src/components/Towers.js
@@ -9,7 +9,8 @@ class Towers extends Component {
       b: [],
       c: []
     }, 
-    userClick: 0
+    startStack: null,
+    error: ''
   };
 
   renderRings(letter) {
@@ -19,8 +20,57 @@ class Towers extends Component {
     })
   }
 
+  isValidMove(endStackLetter){
+    const endStack = this.state.stacks[endStackLetter];
+
+    if(endStack.length){
+      const startStack = this.state.stacks[this.state.startStack];
+      const startStackLastItem = startStack[startStack.length - 1];
+      const endStackLastItem = endStack[endStack.length - 1];
+
+      return startStackLastItem < endStackLastItem
+    }
+    return !endStack.length
+  }
+
   handleUserClick=(stack)=>{
-    console.log("here", stack)
+
+    if(!this.state.startStack){
+      this.setState({startStack: stack})
+    }else{
+      if(this.isValidMove(stack)){
+        const endStackLetter = stack;
+        const newStartStack = [...this.state.stacks[this.state.startStack]];
+        const poppedPiece = newStartStack.pop();
+        const newEndStack = [...this.state.stacks[endStackLetter]];
+        newEndStack.push(poppedPiece);
+        const newStacks = this.state.stacks;
+        newStacks[this.state.startStack] = newStartStack;
+        newStacks[endStackLetter] = newEndStack;
+        this.checkForWin(newStacks);
+        this.setState({stacks: newStacks, error: '', startStack: null});
+      }else{
+        this.setState({error: 'Illegal move, click a valid location'})
+      }
+    }
+  }
+
+  checkForWin(newStacks){
+    console.log(newStacks.a.length)
+    if(newStacks.b.length === 4 || newStacks.c.length === 4){
+      alert("you win!")
+    }
+  }
+
+
+  renderStacks(){
+    return Object.keys(this.state.stacks).map((stack, index) => {
+      return (
+        <div key="index" onClick={() => this.handleUserClick(stack)}>
+          Stack {stack} {this.renderRings(stack)}
+        </div>
+      )
+    })
   }
 
   render(){
@@ -32,9 +82,11 @@ class Towers extends Component {
     return(
       <div style={stackStyle}>
       <h1>Towers of Hanoi</h1>
-        <div onClick={() => this.handleUserClick('a')}>Stack A {this.renderRings('a')}</div>
-        <div onClick={() => this.handleUserClick('b')}>Stack B {this.renderRings('b')}</div>
-        <div onClick={() => this.handleUserClick('c')}>Stack C {this.renderRings('c')}</div>
+        {this.renderStacks()}
+        <div>
+          {this.state.error}
+          Current Stack: {this.state.startStack}
+        </div>
       </div>
     )
   }
