d8c96ded1136bfe69a1df543a4e130e07fd08abe
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-07 21:45:09 -0600

    minor styling

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -20,10 +20,19 @@ class Clock extends Component {
   }
 
   render() {
+
+    const timeStyle ={
+      color: "#074A54",
+      border: "6px solid black",
+      borderRadius: "4px",
+      padding: "15px",
+      width: "100%", 
+      fontSize: "50px",
+      marginTop: "40px"
+    }
+
     return (
-    <div>
-        {this.state.time.toLocaleTimeString()}
-    </div>
+        <div style={timeStyle}>{this.state.time.toLocaleTimeString()}</div>
     )
   }
 }
