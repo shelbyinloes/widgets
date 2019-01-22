e750eec3da75511dfe849369bf52ce43ff8bdb07
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-21 21:15:22 -0600

    celcius/fahrenheit + lifecyle method

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -11,14 +11,18 @@ class Weather extends Component {
         country: undefined,
         humidity: undefined,
         description: undefined,
+        unit: "metric",
+        unitLetter: "C",
         error: undefined
       }
+
     
       getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}&units=imperial`);
+        const unit = this.state.unit;
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}&units=${unit}`);
         const data = await api_call.json();
     
         if (city && country){
@@ -37,9 +41,36 @@ class Weather extends Component {
             country: undefined,
             humidity: undefined,
             description: undefined,
+            unit: undefined,
             error: "Please enter correct values."
           });
         }
+      }
+
+      handleRadioUnit = changeEvent => {
+        let letter;
+        if(changeEvent.target.value === "metric"){
+          letter = "C"
+        }else{
+          letter = "F"
+        }
+        this.setState({
+          unit: changeEvent.target.value,
+          unitLetter: letter
+        })
+      }
+
+      componentWillUnmount(){
+        this.setState({
+          temp: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          unit: "metric",
+          unitLetter: "C",
+          error: undefined
+        })
       }
     
       render() {
@@ -66,12 +97,26 @@ class Weather extends Component {
                 <form onSubmit={this.getWeather}>
                     <input type="text" name="city" placeholder="City..."></input>
                     <input type="text" name="country" placeholder="Country..."></input>
-                    <button>Get Weather</button>
+                    <br></br>
+                    <input 
+                      type="radio" 
+                      value="metric"
+                      checked={this.state.unit === "metric"}
+                      onChange={this.handleRadioUnit}
+                    /><span> Celcius  </span>
+                    <input
+                      type="radio"
+                      value="imperial"
+                      checked={this.state.unit === "imperial"}
+                      onChange={this.handleRadioUnit}
+                    /><span> Fahrenheit</span>
+                    <br></br>
+                    <button type="submit">Get Weather</button>
                 </form>
             </div>
             <div style={weatherInfo}>
                 {this.state.city && this.state.country && <p><span >Location:</span> {this.state.city}, {this.state.country}</p>}  
-                {this.state.temp && <p><span >Temperature:</span> {this.state.temp} F</p>}
+                {this.state.temp && <p><span >Temperature:</span> {this.state.temp} {this.state.unitLetter} </p>}
                 {this.state.humidity && <p><span >Humidity:</span> {this.state.humidity}%</p>}
                 {this.state.description && <p><span >Conditions:</span> {this.state.description}</p>}
                 {this.state.error && <p>{this.state.error}</p>}
