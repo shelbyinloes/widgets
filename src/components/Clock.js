f2e1d576f5ec1ef98ebe7403ab98b0f7bdf1e812
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-22 14:22:49 -0600

    taking out unmount

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -19,10 +19,6 @@ class Clock extends Component {
     setInterval(() => this.currentTime(), 1000)
   }
 
-  componentWillUnmount() {
-    window.clearTimeout(this.currentTime)
-  }
-
   render() {
 
     const timeStyle ={
