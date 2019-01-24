65e7a6050948974188d0dda4225a458ca9d73bcc
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-24 11:18:56 -0600

    timer working, need to add pick a date functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,42 +1,109 @@
 import React, { Component } from 'react';
-import { Grid, Col } from 'react-bootstrap';
 
 class Timer extends Component {
-
   constructor() {
-    super()
-    this.state={
-      time: 5
+    super();
+
+    this.state = {
+      days: 4,
+      hours: 0,
+      min: 0,
+      sec: 0,
     }
   }
 
-  getNumber = (e) => {
-      e.preventDefault();
-      const inputNumber = e.target.elements.inputNumber.value;
+  componentDidMount() {
+    // update every second
+    this.interval = setInterval(() => {
+      const date = this.calculateCountdown(this.date);
+      date ? this.setState(date) : this.stop();
+    }, 1000);
+  }
 
-      this.setState({
-          time: inputNumber
-      })
+  componentWillUnmount() {
+    this.stop();
+  }
+
+  calculateCountdown(endDate) {
+    let diff = (Date.parse(new Date(1548999663000)) - Date.parse(new Date())) / 1000;
+    // console.log(Date.parse(new Date()))
+    // console.log(new Date())
+
+    // clear countdown when date is reached
+    if (diff <= 0) return false;
+
+    const timeLeft = {
+      years: 0,
+      days: 0,
+      hours: 0,
+      min: 0,
+      sec: 0,
+      millisec: 0,
+    };
+
+    // calculate time difference between now and expected date
+    if (diff >= (365.25 * 86400)) { // 365.25 * 24 * 60 * 60
+      timeLeft.years = Math.floor(diff / (365.25 * 86400));
+      diff -= timeLeft.years * 365.25 * 86400;
+    }
+    if (diff >= 86400) { // 24 * 60 * 60
+      timeLeft.days = Math.floor(diff / 86400);
+      diff -= timeLeft.days * 86400;
+    }
+    if (diff >= 3600) { // 60 * 60
+      timeLeft.hours = Math.floor(diff / 3600);
+      diff -= timeLeft.hours * 3600;
+    }
+    if (diff >= 60) {
+      timeLeft.min = Math.floor(diff / 60);
+      diff -= timeLeft.min * 60;
+    }
+    timeLeft.sec = diff;
+
+    return timeLeft;
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  addLeadingZeros(value) {
+    value = String(value);
+    while (value.length < 2) {
+      value = '0' + value;
+    }
+    return value;
   }
 
   render() {
+    const countDown = this.state;
 
     return (
-        <Grid style={{backgroundColor: 'white'}}>
-            <Col sm={12}>
-                <form onSubmit={this.getNumber}>
-                <h3>Input time:</h3>
-                <input type="number" name="inputNumber" required />
-                <button type="submit">Sumbit</button>
-                </form>
-            </Col>
-            <Col sm={12}>
-                <h1>{this.state.time}</h1>
-            </Col>
-     
-        </Grid>
-    )
+      <div className="Countdown" style={{backgroundColor: 'white'}}>
+
+              <strong>{this.addLeadingZeros(countDown.days)}</strong>
+              <span>{countDown.days === 1 ? 'Day' : 'Days'}</span>
+
+            <strong>{this.addLeadingZeros(countDown.hours)}</strong>
+            <span>Hours</span>
+
+            <strong>{this.addLeadingZeros(countDown.min)}</strong>
+            <span>Min</span>
+
+
+            <strong>{this.addLeadingZeros(countDown.sec)}</strong>
+            <span>Sec</span>
+      </div>
+    );
   }
 }
+
+// Timer.propTypes = {
+//   date: PropTypes.string.isRequired
+// };
+
+// Timer.defaultProps = {
+//   date: new Date()
+// };
 
 export default Timer;
